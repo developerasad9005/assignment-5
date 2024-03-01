b58a5ff93ff49d982d0a18c4765b1ba6c779b40e
Programmer Asad <asaduzzamanasad9005@gmail.com>
2024-03-02 03:36:08 +0600

    added coupon system

--- app.js
+++ app.js
@@ -29,11 +29,56 @@ function clickButton(idname){
     const grandtotalsum = ticketContainerLenth * 550; 
     prizeUpdate.innerText = totalPrizeSum;
     grandtotalUpdate.innerText = grandtotalsum;
-
     }
     else{
         alert("You have already selected maximum number of Seats");
     }
 }
+
+document.getElementById('collectCoupon').addEventListener('keyup', function(event){
+    const text = event.target.value;
+    const deleteBtn = document.getElementById('apply');
+    if(text === "NEW15" || text ==="Couple 20"){
+        deleteBtn.removeAttribute('disabled');
+    }
+    else{
+        deleteBtn.setAttribute('disabled', true);
+    }
+})
+document.getElementById('apply').addEventListener('click', function(){
+    const coupon = document.getElementById('collectCoupon').value;
+    if(coupon == "NEW15"){
+        let totalPrize = document.getElementById('totalPrizeUpdate').innerText;
+        let totalPrizeNum = Number(totalPrize);
+        let grandtotal = document.getElementById('grandTotalUpdate');
+        let grandtotalValue = grandtotal.innerText;
+        let grandTotalSum = (totalPrizeNum * 15)/100;
+        const discountBox = document.getElementById('discount');
+        grandtotal.innerText = grandtotalValue - grandTotalSum;
+        let addedChild = document.createElement('h1');
+        addedChild.innerText = "  with 15% Discount";
+        discountBox.appendChild(addedChild);
+        discountBox.classList.add('font-bold');
+        discountBox.classList.add('text-lg');
+        const hidden = document.getElementById('couponHidden');
+        hidden.classList.add('hidden')
+    }
+    if (coupon == "Couple 20") {
+        let totalPrize = document.getElementById('totalPrizeUpdate').innerText;
+        let totalPrizeNum = Number(totalPrize);
+        let grandtotal = document.getElementById('grandTotalUpdate');
+        let grandtotalValue = grandtotal.innerText;
+        let grandTotalSum = (totalPrizeNum * 20)/100;
+        const discountBox = document.getElementById('discount');
+        grandtotal.innerText = grandtotalValue - grandTotalSum;
+        let addedChild = document.createElement('h1');
+        addedChild.innerText = "  with 20% Discount";
+        discountBox.appendChild(addedChild);
+        discountBox.classList.add('font-bold');
+        discountBox.classList.add('text-lg');
+        const hidden = document.getElementById('couponHidden');
+        hidden.classList.add('hidden')
+    }
+})
 
 
